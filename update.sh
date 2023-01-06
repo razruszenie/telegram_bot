8afc1f16e0636d18930102b234f45574264685c1
Aleh Yarkavets <mr.yandex2013@icloud.com>
2023-01-06 13:21:33 +0300

    Dockerfile update

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-rm -rf apbot/
+rm -rf telegram_bot/
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
 rm -rf server middleware
 rm README.md Dockerfile docker-compose.yml package-lock.json package.json
 git clone https://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@github.com/razruszenie/telegram_bot
-mv -v ~/apbot/apbot/* ~/apbot
+mv -v ~/apbot/telegram_bot/* ~/apbot
 docker-compose up -d --build
