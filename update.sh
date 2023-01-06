df93eef15c4eb2e07e1344fb36eef55a1aed53aa
Aleh Yarkavets <mr.yandex2013@icloud.com>
2023-01-06 13:44:13 +0300

    Dockerfile update

--- update.sh
+++ update.sh
@@ -5,6 +5,3 @@ docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
 rm -rf server middleware
 rm Dockerfile docker-compose.yml package-lock.json package.json
-#git clone https://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@github.com/razruszenie/telegram_bot
-#mv -v ~/apbot/telegram_bot/* ~/apbot
-#docker-compose up -d --build
