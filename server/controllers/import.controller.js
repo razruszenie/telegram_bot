ea9f15e992830ff36fe68193804808a242ab0f71
Aleh Yarkavets <mr.yandex2013@icloud.com>
2023-03-08 01:34:11 +0300

    Req update

--- server/controllers/import.controller.js
+++ server/controllers/import.controller.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 
 module.exports.drom = async (req, res) => {
 
+  console.log(req.headers.host)
+
   res.json('ok')
 
   const response = await axios.get("https://bampart.com/api/export/bamper?token=9dc4bd31-61b8-42c0-9769-63d0bd1d50d0",
